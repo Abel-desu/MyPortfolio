dd66a25076b9f1aed2f02298c0e0a56fb63d5031
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-05-19 17:12:44 +0300

    creating validation file

--- validation.js
+++ validation.js
@@ -1,1 +1,13 @@
-
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+form.addEventListener('submit', (e) => {
+  const messages = [];
+  if (email.value.match(/^[a-z@.0-9-_]*$/)) {
+    messages.push('your email is correct');
+  } else {
+    e.preventDefault();
+    messages.push('Enter the email in lower case to continue');
+    errorElement.innerText = messages.join(', ');
+  }
+});
