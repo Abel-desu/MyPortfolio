6be762cb256022ec9562f76a39268f1a5ee83272
Farida-Hamid <33801345+Farida-Hamid@users.noreply.github.com>
2022-05-20 10:42:22 +0300

    Add project content

--- js/script.js
+++ js/script.js
@@ -35,3 +35,53 @@ open.addEventListener('click', () => {
     window.removeChild(popup);
   });
 });
+
+// Project popup window
+ const seeProject = document.querySelectorAll(''); // Add your see project button class
+
+ const projects = [
+   {
+     title: 'Tonic',
+     devs: ['CANOPY', '•', 'Back End Dev', '•', '2015'],
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+     tags: ['html', 'css', 'javascript', ''],
+     image: '', // Add adress to project snapshot
+     liveButton: 'See Live', // This is a new button you don't need to change it
+     sourceButton: 'See Source',  // This is a new button you don't need to change it
+     live: '', // Download the photo in the see live button and add it's adress here 
+     source: '', // Download the photo in the source button and add it's adress here 
+   },
+   {
+     title: 'Multi-Post Stories',
+     devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+     tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+     image: '',  // Add adress to project snapshot
+     liveButton: 'See Live',
+     sourceButton: 'See Source',
+     live: '', // Download the photo in the see live button and add it's adress here 
+     source: '', // Download the photo in the source button and add it's adress here 
+   },
+   {
+     title: 'Facebook 360',
+     devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+     tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+     image: '',  // Add adress to project snapshot
+     liveButton: 'See Live',
+     sourceButton: 'See Source',
+     live: '', // Download the photo in the see live button and add it's adress here 
+     source: '', // Download the photo in the source button and add it's adress here 
+   },
+   {
+     title: 'Uber Navigation',
+     devs: ['Uber', '•', 'Lead Developer', '•', '2018'],
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+     tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+     image: '',  // Add adress to project snapshot
+     liveButton: 'See Live',
+     sourceButton: 'See Source',
+     live: '', // Download the photo in the see live button and add it's adress here 
+     source: '', // Download the photo in the source button and add it's adress here 
+   },
+ ];
