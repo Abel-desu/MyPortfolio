2dfb6550996119738cd239ecbe5550827ee36fff
Farida-Hamid <33801345+Farida-Hamid@users.noreply.github.com>
2022-05-21 14:32:18 +0300

    Store user input

--- Local-storage.js
+++ Local-storage.js
@@ -2,3 +2,8 @@
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const message = document.querySelector('#msg');
+
+function storage(name, email, message) {
+  const saver = JSON.stringify({ name, email, message });
+  localStorage.setItem('data', saver);
+}
