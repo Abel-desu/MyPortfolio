2e2a1522510d738cc58d3ffa91bc9b9cda5992c7
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-05-21 21:13:26 +0300

    add popup files

--- js/script.js
+++ js/script.js
@@ -37,57 +37,62 @@ open.addEventListener('click', () => {
 
 // Project popup window
 const seeProject = document.querySelectorAll('.show-project'); // Add your see project button class
-
+const mobileProject = document.querySelectorAll('.mobile-project');
 const projects = [
   {
+    id: 1,
     title: 'Tonic',
     devs: ['CANOPY', '•', 'Back End Dev', '•', '2015'],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    tags: ['html', 'css', 'javascript', ''],
-    image: 'images/Snapshootortfolio.jpg', // Add adress to project snapshot
-    liveButton: 'images/', // This is a new button you don't need to change it
-    sourceButton: 'images/', // This is a new button you don't need to change it
-    live: 'images/', // Download the photo in the see live button and add it's adress here
-    source: 'images/', // Download the photo in the source button and add it's adress here
+    tags: ['html', 'Ruby on rails', 'css', 'javascript', 'github', 'Bootstraps'],
+    image: 'images/Snapshoot-popup.png', // Add adress to project snapshot
+    liveButton: 'live button', // This is a new button you don't need to change it
+    sourceButton: 'see source', // This is a new button you don't need to change it
+    live: 'images/icon.png', // Download the photo in the see live button and add it's adress here
+    source: 'images/Icon-GitHub.png', // Download the photo in the source button and add it's adress here
   },
   {
+    id: 2,
     title: 'Multi-Post Stories',
     devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    tags: ['html', 'Ruby on rails', 'css', 'javascript'],
-    image: '', // Add adress to project snapshot
-    liveButton: 'See Live',
+    tags: ['html', 'Ruby on rails', 'css', 'javascript', 'github', 'Bootstraps'],
+    image: 'images/4.png',
+    liveButton: 'live button',
     sourceButton: 'See Source',
-    live: '', // Download the photo in the see live button and add it's adress here
-    source: '', // Download the photo in the source button and add it's adress here
+    live: 'images/icon.png', // Download the photo in the see live button and add it's adress here
+    source: 'images/Icon-GitHub.png', // Download the photo in the source button and add it's adress here
   },
   {
+    id: 3,
     title: 'Facebook 360',
     devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    tags: ['html', 'Ruby on rails', 'css', 'javascript'],
-    image: '', // Add adress to project snapshot
-    liveButton: 'See Live',
+    tags: ['html', 'Ruby on rails', 'css', 'javascript', 'github', 'Bootstraps'],
+    image: 'images/d3.png', // Add adress to project snapshot
+    liveButton: 'live button',
     sourceButton: 'See Source',
-    live: '', // Download the photo in the see live button and add it's adress here
-    source: '', // Download the photo in the source button and add it's adress here
+    live: 'images/icon.png', // Download the photo in the see live button and add it's adress here
+    source: 'images/Icon-GitHub.png', // Download the photo in the source button and add it's adress here
   },
   {
+    id: 4,
     title: 'Uber Navigation',
     devs: ['Uber', '•', 'Lead Developer', '•', '2018'],
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    tags: ['html', 'Ruby on rails', 'css', 'javascript'],
-    image: '', // Add adress to project snapshot
-    liveButton: 'See Live',
+    tags: ['html', 'Ruby on rails', 'css', 'javascript', 'github', 'Bootstraps'],
+    image: 'images/d4.png', // Add adress to project snapshot
+    liveButton: 'live button',
     sourceButton: 'See Source',
-    live: '', // Download the photo in the see live button and add it's adress here
-    source: '', // Download the photo in the source button and add it's adress here
+    live: 'images/icon.png', // Download the photo in the see live button and add it's adress here
+    source: 'images/Icon-GitHub.png', // Download the photo in the source button and add it's adress here
   },
 ];
 
 function display(index) {
   const main = document.querySelector('main');
   const {
+    id,
     title,
     devs,
     description,
@@ -107,14 +112,16 @@ function display(index) {
   const tag2 = tags[1];
   const tag3 = tags[2];
   const tag4 = tags[3];
-
+  const tag5 = tags[4];
+  const tag6 = tags[5];
   const projectPopup = document.createElement('div');
   projectPopup.className = 'project-popup';
   projectPopup.innerHTML = `
      <div class="desktop-window">
+     <div class="all-desktop-popup"
        <div class="desktop-top-popup">
          <h2>${title}</h2>
-         <button type='button' class="close-button">X</button>
+         <button type='button' class="X-button">X</button>
        </div>
        <ul class="devls"> 
          <li class="dev1">${dev1}</li> 
@@ -123,15 +130,23 @@ function display(index) {
          <li class="dev4">${dev4}</li>
          <li class="dev5">${dev5}</li>
        </ul>
-       <img class="desktop-popup-snapshot" src=${image} alt="project 1 snapshot">  
+       <img class="desktop-popup-snapshot image-${id}" src=${image} alt="project 1 snapshot">  
+       <div class="popup-footer">
        <p class="p-popup">${description}</p> 
-       <ul class="prog-languages">  
+       <section class=lang-button>
+        <ul class="prog-languages">  
+        <ul class="ul">
          <li class="lang">${tag1}</li>   
          <li class="lang">${tag2}</li>  
          <li class="lang">${tag3}</li> 
+         </ul>
+         <ul class="ul1">
          <li class="lang">${tag4}</li>   
+         <li class="lang">${tag5}</li>   
+         <li class="lang">${tag6}</li>  
+         </ul>
        </ul>
-       <hr>
+       <div class ="desktopup-button">  
        <button type='button' class="popup-button">
          ${liveButton}
          <img class="logo-popup-button" src=${live}> 
@@ -140,14 +155,20 @@ function display(index) {
          ${sourceButton}
          <img class="logo-popup-button" src=${source}>  
        </button>
+       </section>
+     </div>
+     </div>
+     </div>
      </div>`;
-
   main.appendChild(projectPopup);
-  const close = document.querySelector('.close-button');
+  const close = document.querySelector('.X-button');
   close.addEventListener('click', () => {
     main.removeChild(projectPopup);
   });
 }
 for (let i = 0; i < seeProject.length; i += 1) {
   seeProject[i].addEventListener('click', () => display(i));
+}
+for (let i = 0; i < mobileProject.length; i += 1) {
+  mobileProject[i].addEventListener('click', () => display(i));
 }
