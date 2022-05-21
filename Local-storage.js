accaf3d7dfcd3d76eb3c429b6fa8d3cb1679d03f
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-05-21 14:34:11 +0300

    Retrieve the code

--- Local-storage.js
+++ Local-storage.js
@@ -7,3 +7,18 @@ function storage(name, email, message) {
   const saver = JSON.stringify({ name, email, message });
   localStorage.setItem('data', saver);
 }
+function retrieve() {
+  const data = JSON.parse(localStorage.getItem('data'));
+
+  if (data) {
+    name.value = data.name;
+    email.value = data.email;
+    message.value = data.message;
+  }
+
+  name.addEventListener('input', () => storage(name.value, email.value, message.value));
+  email.addEventListener('input', () => storage(name.value, email.value, message.value));
+  message.addEventListener('input', () => storage(name.value, email.value, message.value));
+}
+
+retrieve();
